229281ff508ea8ab2f6a6400098d574d6bed9f52
Abdoul Karim DIALLO <akdiallo@gainde2000.sn>
2023-06-12 23:25:44 +0000

    debut de configuration de keycloak front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,28 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+/*Cette fonction permet de demarer keycloak au demarage*/
+export function kcFactory(kycloakService: KeycloakService) {
+  /*Return une promesse*/
+  return () => {
+    kycloakService.init({
+      config: {
+        realm: "wallet-realm",
+        clientId: "wallet-client",
+        url: "http://localhost:8080"
+      },
+      initOptions: {
+        onLoad: "login-required",
+        checkLoginIframe: true
+      }
+    }
+    )
+  }
+}
+
 
 @NgModule({
   declarations: [
@@ -10,9 +30,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [{
+    provide: APP_INITIALIZER,
+    deps: [KeycloakService],
+    useFactory: kcFactory,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
